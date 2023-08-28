fb378da94f59d8137ede6c5d037eea97fe5f2449
arhammusheer <arhammusheer@gmail.com>
2023-08-28 17:31:08 -0400

    Fixed bus departure error

--- src/app.js
+++ src/app.js
@@ -40,9 +40,15 @@ app.get("/bus", async (req, res) => {
 
     bus = JSON.stringify(busData);
 
+    // Expire 30s after first bus left. or after 30 mins whichever is less
+    const expiresAt =
+      busData &&
+      busData.route_departures[0].itineraries[0].schedule_items[0]
+        .departure_time + 30;
+
     await client.set("bus:31", bus, {
       // Expire in 30 mins
-      EX: 60 * 30, 
+      EXAT: expiresAt > Date.now() + 1800 ? Date.now() + 1800 : expiresAt,
     });
 
     res.setHeader("X-data-source", "Transit-API");
@@ -62,3 +68,61 @@ app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
 export default app;
+
+const EXAMPLE_BUS = {
+  route_departures: [
+    {
+      global_route_id: "PVTAMA:116200",
+      itineraries: [
+        {
+          branch_code: "",
+          direction_headsign: "Sunderland",
+          direction_id: 0,
+          headsign: "Sunderland",
+          merged_headsign: "Sunderland",
+          schedule_items: [
+            {
+              departure_time: 1693255980,
+              is_cancelled: false,
+              is_real_time: false,
+              rt_trip_id: "4017495-UM2223-UMTS-Weekday-78",
+              scheduled_departure_time: 1693255980,
+              trip_search_key: "PVTAMA:43094228:5:0:16",
+              wheelchair_accessible: 1,
+            },
+            {
+              departure_time: 1693258080,
+              is_cancelled: false,
+              is_real_time: false,
+              rt_trip_id: "4017523-UM2223-UMTS-Weekday-78",
+              scheduled_departure_time: 1693258080,
+              trip_search_key: "PVTAMA:43094228:5:0:17",
+              wheelchair_accessible: 1,
+            },
+            {
+              departure_time: 1693260180,
+              is_cancelled: false,
+              is_real_time: false,
+              rt_trip_id: "4017496-UM2223-UMTS-Weekday-78",
+              scheduled_departure_time: 1693260180,
+              trip_search_key: "PVTAMA:43094228:5:0:18",
+              wheelchair_accessible: 1,
+            },
+          ],
+        },
+      ],
+      mode_name: "Bus",
+      real_time_route_id: "20031",
+      route_color: "ea6083",
+      route_long_name: "Sunderland  /  South Amherst",
+      route_network_id: "PVTA|Boston",
+      route_network_name: "PVTA",
+      route_short_name: "31",
+      route_text_color: "ffffff",
+      route_type: 3,
+      sorting_key: "31",
+      tts_long_name: "Sunderland  /  South Amherst",
+      tts_short_name: "31",
+    },
+  ],
+};
