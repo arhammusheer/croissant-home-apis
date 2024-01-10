bfad8d811419e45dd4af3e775b8b3cfe687b5734
arhammusheer <arhammusheer@gmail.com>
2024-01-10 16:58:07 -0500

    Added prom logging

--- src/utils/prometheus.ts
+++ src/utils/prometheus.ts
@@ -1,5 +1,7 @@
 import express_prom_bundle from "express-prom-bundle";
+import { logger } from "./logger";
 
+const LOG_OWNER = "prometheus";
 const ENABLE_PROMETHEUS = process.env.ENABLE_PROMETHEUS === "true";
 
 const prometheusMiddleware = express_prom_bundle({
@@ -7,6 +9,10 @@ const prometheusMiddleware = express_prom_bundle({
   includePath: true,
   includeStatusCode: true,
 });
+
+if (ENABLE_PROMETHEUS) {
+  logger.info(LOG_OWNER, "Prometheus enabled");
+}
 
 // Prometheus metrics
 export const prometheus = ENABLE_PROMETHEUS ? prometheusMiddleware : (_req: any, _res: any, next: any) => next();
